c0a5b498c473c36d80bbede79efdb445fa44f05d
AnneLund <89774650+AnneLund@users.noreply.github.com>
2022-08-17 10:50:52 +0200

    express

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const port = process.env.port || 3001;
 const app = express();
 
+app.get("/api", (req, res) => {
+    res.json({message: "Hej fra serveren!"})
+})
+
 app.listen(port, () => {
     console.log(`Serveren kører på port ${port}`)
 })
